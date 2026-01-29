1b4ec37101ecfab7ff238136e24d26acbb2e9e63
Ayushdeval821 <devalayush25@gmail.com>
2026-01-29 10:54:47 +0530

    apdate

--- routes/userinfoRouteHandler.js
+++ routes/userinfoRouteHandler.js
@@ -1,143 +1,53 @@
-// // import fs from 'fs';
+import fs from "fs";
 
-// // const usersInfoRouteHandler=(req,res)=>{
-// //     // console.log("Hii");
-// //     const userData=[];
-// //     req.on("data",(chunk)=>
-// //     {
-// //         userData.push(chunk)
-// //     })
-// //     req.on('end',()=>{
-// //         const parsedUserData = Buffer.concat(userData).toString();
-// //         console.log(parsedUserData);
-// //         // res.write(parsedUserData);
-// //         // return res.end();
-
-// //         console.log(parsedUserData.split("&")[0].split('=')[1]);
-// //         const fileName = parsedUserData.split('&')[0].split('=')[1];
-// //         fs.writeFileSync(`./Users/${fileName}.txt`,parsedUserData);
-// //         const files = fs.readdirSync("./Users");
-// //         console.log(fileData);
-// //         files.map((files))=>{
-// //             const fileData=fs.readFileSync(`./Users/${files}`,"utf8");
-// //             console.log(fileData);
-// //         };
-
-
-// //     });
-
-
-// // export default usersInfoRouteHandler;
-
-
-// // import fs from 'fs';
-
-// // const usersInfoRouteHandler=(req,res)=>{
-// //     // console.log("Hii");
-// //     const userData=[];
-// //     req.on("data",(chunk)=>
-// //     {
-// //         userData.push(chunk)
-// //     })
-// //     req.on('end',()=>{
-// //         const parsedUserData = Buffer.concat(userData).toString();
-// //         console.log(parsedUserData);
-// //         // res.write(parsedUserData);
-// //         // return res.end();
-
-// //         console.log(parsedUserData.split("&")[0].split('=')[1]);
-// //         const fileName = parsedUserData.split('&')[0].split('=')[1];
-// //         fs.writeFileSync(`./Users/${fileName}.txt`,parsedUserData);
-// //         const files = fs.readdirSync("./Users");
-// //         console.log(fileData);
-// //         files.map((files))=>{
-// //             const fileData=fs.readFileSync(`./Users/${files}`,"utf8");
-// //             console.log(fileData);
-// //         };
-
-
-// //     })
-// // }
-
-// // export default usersInfoRouteHandler;
-
-// import fs from 'fs';
-// const usersInfoRouteHandler = (req, res) =>
-// {
-//    const userdata =[];
-//     req.on('data', (chunk) =>
-//     {
-//         userdata.push(chunk);
-//     })
-//     req.on('end', () =>
-//     {
-//         const parsedData = Buffer.concat(userdata).toString();
-//         console.log(parsedData.split('&')[0].split('=')[1]);
-//         const fileName = parsedData.split('&')[0].split('=')[1];
-//         fs.writeFileSync(`./Users/${fileName}.txt`, parsedData);
-//         const file = fs.readdirSync('./Users','utf-8');
-//         console.log(file);
-//         file.map((files)=>{
-//             const filedata = fs.readFileSync(`./Users/${files}`, 'utf-8');
-//             console.log(filedata);
-//         })
-//     })
-// };
-
-// export default usersInfoRouteHandler;
-
-// import fs from 'fs';
-
-// const usersInfoRouteHandler=(req,res)=>{
-//     // console.log("Hii");
-//     const userData=[];
-//     req.on("data",(chunk)=>
-//     {
-//         userData.push(chunk)
-//     })
-//     req.on('end',()=>{
-//         const parsedUserData = Buffer.concat(userData).toString();
-//         console.log(parsedUserData);
-//         // res.write(parsedUserData);
-//         // return res.end();
-
-//         console.log(parsedUserData.split("&")[0].split('=')[1]);
-//         const fileName = parsedUserData.split('&')[0].split('=')[1];
-//         fs.writeFileSync(`./Users/${fileName}.txt`,parsedUserData);
-//         const files = fs.readdirSync("./Users");
-//         console.log(fileData);
-//         files.map((files))=>{
-//             const fileData=fs.readFileSync(`./Users/${files}`,"utf8");
-//             console.log(fileData);
-//         };
-
-
-//     })
-// }
-
-// export default usersInfoRouteHandler;
-
-import fs from 'fs';
-const usersInfoRouteHandler = (req, res) =>
-{
-   const userdata =[];
-    req.on('data', (chunk) =>
-    {
-        userdata.push(chunk);
-    })
-    req.on('end', () =>
-    {
-        const parsedData = Buffer.concat(userdata).toString();
-        console.log(parsedData.split('&')[0].split('=')[1]);
-        const fileName = parsedData.split('&')[0].split('=')[1];
-        fs.writeFileSync(`./Users/${fileName}.txt`, parsedData);
-        const file = fs.readdirSync('./Users','utf-8');
-        console.log(file);
-        file.map((files)=>{
-            const filedata = fs.readFileSync(`./Users/${files}`, 'utf-8');
-            console.log(filedata);
-        })
-    })
-};
+const usersInfoRouteHandler=(req,res)=>{
+   const userData=[];
+   req.on('data',(chunk)=>{
+     userData.push(chunk);
+   }) 
+   req.on('end',()=>{
+    const parsedUserData=Buffer.concat(userData).toString();
+    console.log(parsedUserData.split('&')[0].split('=')[1]);
+    const fileName=parsedUserData.split('&')[0].split('=')[1];
+    const upperHtml=`
+    <html>
+      <head>
+        <title>
+          Users Info Page
+        </title>
+      </head>
+      <body>
+        <h1>
+          Hii , Welcome to Our Team !!!
+        </h1>
+        <div style="display:flex">
+    `  
+    let middleHtml=``;
+    const lowerHtml=`
+          </div>
+      </body>
+    </html>
+    `
+    fs.writeFileSync(`./Users/${fileName}.txt`,parsedUserData);
+    const files=fs.readdirSync("./Users");
+    console.log(files);
+    files.map((file)=>{
+        const fileData=fs.readFileSync(`./Users/${file}`,"utf8");
+        console.log(fileData);
+        middleHtml=middleHtml+`
+        <div style="margin:10px; padding:10px; border:2px solid black; border-radius:5px;width:500px; background-color:grey">
+        <h1>
+        ${fileData.split('&')[0].split('=')[1]}
+        </h1>
+        <p>
+        ${fileData.split('&')[1].split('=')[1]}
+        </p>
+        </div>
+        `
+    });
+    res.write(upperHtml+middleHtml+lowerHtml);
+    return res.end();
+   })
+}
 
 export default usersInfoRouteHandler;
